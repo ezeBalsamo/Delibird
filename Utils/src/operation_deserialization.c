a9d1acd69ce370d0c865a6ba507cce2d0f4891b5
Franco <francogiannotti115@gmail.com>
2020-04-26 20:39:59 -0300

    Agrego funciones de deserializacion, Caught y Get pokemon, quedo la parte de serializar pero estoy teniendo problemas con el make file

--- Utils/src/operation_deserialization.c
+++ Utils/src/operation_deserialization.c
@@ -103,5 +103,34 @@ t_request* deserialize_catch_pokemon(void* serialized_structure) {
     printf("request structure: pokemon: %s, pos x: %d, pos y: %d, id_mensaje: %d.\n", pokemon_name, pos_x, pos_y, message_id);
 }
 
-t_request* deserialize_caught_pokemon(void* serialized_structure){return NULL;}
-t_request* deserialize_get_pokemon(void* serialized_structure){return NULL;}
+t_request* deserialize_caught_pokemon(void* serialized_structure){
+
+    uint32_t message_id;
+    uint32_t caught_status;    // 0/1 - FAIL/OK
+    uint32_t offset = 0;
+
+    memcpy(&message_id, serialized_structure + offset, sizeof(uint32_t));
+    offset += sizeof(uint32_t);
+    memcpy(&caught_status, serialized_structure + offset, sizeof(uint32_t));
+
+    printf("request deserialized!\n");
+    printf("request operation number: %d\n", CAUGHT_POKEMON);
+    printf("request structure: : id_mensaje: %d,  caught_status: %d.\n", message_id,  caught_status);
+}
+
+t_request* deserialize_get_pokemon(void* serialized_structure){
+    //Mensaje GET envia unicamente el pokemon
+    uint32_t pokemon_name_length;
+    char* pokemon_name;
+    uint32_t offset = 0;
+
+    memcpy(&pokemon_name_length, serialized_structure + offset, sizeof(uint32_t));
+    offset += sizeof(uint32_t);
+    pokemon_name = malloc(pokemon_name_length);
+    memcpy(pokemon_name, serialized_structure + offset, pokemon_name_length);
+    offset += pokemon_name_length;
+
+    printf("request deserialized!\n");
+    printf("request operation number: %d\n", GET_POKEMON);
+    printf("request structure: pokemon: %s.\n", pokemon_name);
+}
