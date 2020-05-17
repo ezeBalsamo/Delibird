20910d0dac9aff5034abbd49dff88dbfaebb2512
yairtele <ytelezon@est.frba.utn.edu.ar>
2020-05-17 01:04:14 -0300

    Subo entry_point_executor delegando la lógica de ejecución del gameboy.

--- Gameboy/src/gameboy.c
+++ Gameboy/src/gameboy.c
@@ -11,17 +11,7 @@ int main(int arguments_amount, char* arguments[]) {
     initialize_entry_point_connection_builder();
     log_succesful_start_up();
 
-    t_request* request = safe_request();
-    char* ip = process_ip();
-    char* port = process_port();
-
-    int socket_fd = connect_to(ip, port, close_connection_strategy);
-    log_successful_connection();
-    log_about_to_send_request(request);
-    send_structure(request, socket_fd);
-    log_request_sent(request);
-
-    free(request);
+    execute();
 
     free_entry_point_validator();
     free_entry_point_logs_manager();
