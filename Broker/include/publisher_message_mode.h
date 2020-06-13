718aad88376da56aeca548d2f63e2a50eceec4e5
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2020-06-13 00:47:14 -0300

    Agrego funcion allocate_message que le sirve a franco para continuar con su desarrollo.

--- Broker/include/publisher_message_mode.h
+++ Broker/include/publisher_message_mode.h
@@ -8,4 +8,6 @@ t_message_role_identifier* publisher_mode();
 
 void initialize_publisher_message_mode();
 
+void allocate_message(t_identified_message* identified_message);
+
 #endif //DELIBIRD_PUBLISHER_MESSAGE_MODE_H
