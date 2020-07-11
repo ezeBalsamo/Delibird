fb5c82f781e1720752d4f613de5729d8f6e77565
Franco <francogiannotti115@gmail.com>
2020-07-10 23:52:38 -0300

    remuevo funcion de guardado que ya no tiene sentido lo que hace, simplemente ahora son 2 lineas, porque la parte de dividir el bloque para evitar fragmentacion interna en buddy no se puede (se divide en buddies lo mas que se pueda, pero eso se hace antes)

--- Broker/src/buddy_system_message_allocator.c
+++ Broker/src/buddy_system_message_allocator.c
@@ -126,29 +126,6 @@ t_block_information* find_block_to_allocate_message_for_buddy(t_list* blocks_inf
 
 }
 
-t_block_information* save_memory_block_in_block_information_for_buddy(t_block_information* block_information_found, t_memory_block* memory_block_to_save){
-
-    uint32_t memory_size_to_partition = block_information_found->block_size;
-    uint32_t block_size_to_allocate = block_size_for(memory_block_to_save);
-
-    block_information_found->is_free = false;
-    block_information_found->memory_block = memory_block_to_save;
-    block_information_found->block_size = block_size_to_allocate;
-
-    uint32_t memory_size_left = memory_size_to_partition - block_information_found->block_size;
-
-    t_block_information* new_block_information = NULL;
-    if (memory_size_left > 0){
-        new_block_information = safe_malloc(sizeof(t_block_information));
-
-        new_block_information->is_free = true;
-        new_block_information->initial_position = block_information_found->initial_position + block_size_to_allocate;
-        new_block_information->block_size = memory_size_left;
-    }
-
-    return new_block_information;
-}
-
 //LA PAPA DEL BUDDY SYSTEM
 void partition_allocate_message(t_identified_message* message,t_list* blocks_information){
     //logica para guardar un mensaje en memoria
@@ -157,12 +134,9 @@ void partition_allocate_message(t_identified_message* message,t_list* blocks_inf
 
     t_block_information* block_information_found = find_block_to_allocate_message_for_buddy(blocks_information, memory_block_to_save);
 
-    t_block_information* new_block_information = save_memory_block_in_block_information_for_buddy(block_information_found,memory_block_to_save);
+    block_information_found->is_free = false;
+    block_information_found->memory_block = memory_block_to_save;
 
-    if (new_block_information != NULL){
-        int position = block_index_position(block_information_found,blocks_information);
-        list_add_in_index(blocks_information,position+1,(void*) new_block_information);
-    }
     log_succesful_save_message_to_cache(message_request_from_identified_message(message),block_information_found->initial_position);
 }
 
