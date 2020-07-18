cbba454d05fb5302f60404487662a267b0c6c4c2
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2020-07-18 12:58:07 -0300

    listo para la entrega
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- Utils/src/logger.c
+++ Utils/src/logger.c
@@ -42,7 +42,7 @@ t_log* logger_named(char* log_name){
 
 void create_log_named(char* log_name){
     char* log_absolute_path = absolute_path_for_log_named(log_name);
-    t_log* logger = log_create(log_absolute_path, program_name, true, LOG_LEVEL_DEBUG);
+    t_log* logger = log_create(log_absolute_path, program_name, false, LOG_LEVEL_DEBUG);
     dictionary_put(loggers_by_name, log_name, (void*) logger);
 
     free(log_absolute_path);
