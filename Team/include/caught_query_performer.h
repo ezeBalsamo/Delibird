64c7103d43570d46bd8cb57ff329a347b80aa43e
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2020-07-20 18:58:04 -0300

    Revert "fix"
    This reverts commit c0a8318f
    
    Signed-off-by: ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>

--- Team/include/caught_query_performer.h
+++ Team/include/caught_query_performer.h
@@ -5,6 +5,5 @@
 
 void initialize_caught_query_performer();
 t_query_performer* caught_query_performer();
-t_identified_message* arrival_identified_message_of(int ack);
 
 #endif //DELIBIRD_CAUGHT_QUERY_PERFORMER_H
